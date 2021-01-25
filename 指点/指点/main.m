a34543b5268d89d649bf129d552b07c87a33f4e5
ZT <3193152800@qq.com>
2021-01-25 15:08:55 +0800

    字典上传

---
+++ 指点/指点/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  指点
+//
+//  Created by haivera on 2019/4/9.
+//  Copyright © 2019年 haivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
