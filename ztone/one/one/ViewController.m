ae35854cac6d41242a5e6877cd22c12449716f16
ZT <3193152800@qq.com>
2021-01-27 15:20:41 +0800

    添加一个完整的项目

---
+++ ztone/one/one/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  one
+//
+//  Created by coderz on 2021/1/27.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"one";
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+
+@end
