6739e17d3949db401bf11d042c5e32508f3ecab0
ZT <3193152800@qq.com>
2021-01-27 14:58:37 +0800

    添加一个新文件

---
+++ WTModule/one.h
@@ -0,0 +1,19 @@
+//
+//  one.h
+//  iosKnow
+//
+//  Created by coderz on 2020/12/21.
+//  Copyright © 2020 ZhangTeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface one : NSObject
+
++(void)demo;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WTModule/one.m
@@ -0,0 +1,19 @@
+//
+//  one.m
+//  iosKnow
+//
+//  Created by coderz on 2020/12/21.
+//  Copyright © 2020 ZhangTeng. All rights reserved.
+//
+
+#import "one.h"
+
+@implementation one
+
++(void)demo{
+    
+    NSLog(@"demo");
+    
+}
+
+@end
